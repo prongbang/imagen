851afdd334dd85d5c2aa6aaa6646dc3dd7f7a29d
prongbang <prongbang@gmail.com>
2024-06-13 22:14:37 +0700

    feat: generate image

---
+++ android/app/src/main/kotlin/com/inteniquetic/imagen/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.inteniquetic.imagen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_filex/flutter_filex.dart';
+import 'package:image/image.dart' as x;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Text('A4 Image'),
+        ),
+        body: Container(
+          margin: const EdgeInsets.all(16),
+          child: FutureBuilder<Uint8List>(
+            future: _loadAndResizeImage(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final image = snapshot.data!;
+                return Column(
+                  children: [
+                    Expanded(child: Image.memory(image)),
+                    TextButton(
+                      onPressed: () async {
+                        _processSave(image);
+                      },
+                      child: const Text(
+                        'Save',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<Uint8List> _loadAndResizeImage() async {
+    ByteData imageData = await rootBundle.load('assets/images/a4.jpg');
+    Uint8List imageBytes = imageData.buffer.asUint8List();
+    x.Image image = x.decodeImage(imageBytes)!;
+    // x.Image resizedImage = x.copyResize(image, width: A4_WIDTH_PX, height: A4_HEIGHT_PX);
+
+    x.drawString(
+      image,
+      'Your Text Here',
+      font: x.arial48,
+      x: 100,
+      y: 100,
+      wrap: true,
+      color: x.ColorFloat32.rgb(0, 0, 0),
+    );
+
+    List<int> encodeImage = x.encodeJpg(image);
+    return Uint8List.fromList(encodeImage);
+  }
+
+  void _processSave(Uint8List imageBytes) async {
+    x.Image image = x.decodeImage(imageBytes)!;
+    final jpeg = x.encodeJpg(image);
+
+    // Write the PNG formatted data to a file.
+    final directoryUtility = DownloadsDirectoryUtility();
+    final fileXUtility = FileXProvider.create(directoryUtility);
+    final fileX = FileX(
+      prefix: 'imagen-',
+      filename: 'a4',
+      extension: FileX.jpeg,
+      bytes: jpeg,
+    );
+    String path = await fileXUtility.write(fileX);
+    print('path: $path');
+  }
+}
